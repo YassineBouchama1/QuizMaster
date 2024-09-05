7aa1197be29c75bb0067f623aa254a91239887a5
siskoweb <sisko4dev@gmail.com>
2024-09-05 12:14:10 +0100

    trying fix bug db

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-
-const db = require('../config/database');
-
 router.get('/', (req, res) => {
 
 
-  const query = 'SELECT * FROM users';
-  
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Error executing query:', err);
-      res.status(500).send('Error fetching data');
-      return;
-    }
-    
-    res.render('index', { quizes: results });
-  });
+  res.render('index', { quizes: 'hello quiz' });
 });
 
 module.exports = router;
