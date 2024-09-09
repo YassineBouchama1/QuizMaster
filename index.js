8a923ea27aa4a46de346ad389b74b3f961e8c114
siskoweb <sisko4dev@gmail.com>
2024-09-09 16:50:48 +0100

    Refactored :

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const path = require('path');
 const teacherRouter = require('./routes/teacherRoute');
 
-const studentRouter = require('./routes/studentRoute');
-const testRouter = require('./routes/index');
 
 const authRouter = require('./routes/authRoute');
 const guestRouter = require('./routes/guestRoute');
